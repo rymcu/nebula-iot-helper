864035f572609b703080d5915841d82690918ae2
x ronger <ronger-x@outlook.com>
2020-08-26 21:55:02 +0800

    first commit

---
+++ src/background.js
@@ -0,0 +1,113 @@
+'use strict'
+
+import { app, protocol, BrowserWindow, ipcMain } from 'electron'
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+const dgram = require('dgram')
+ipcMain.on('receiving-broadcast', (event, arg) => {
+  // 调用 nodejs 的 dgram 接口接收 udp 数据报
+  // 套接字族为 'udp4'
+  const socket = dgram.createSocket('udp4');
+  console.log(arg)
+
+  // 接收 UDP 数据报消息
+  socket.on('message', (msg, rInfo) => {
+    // 当数据报中数据包含 '10006' 字符串时，将数据报的来源数据发送给渲染进程
+    if (~msg.indexOf('10006')) {
+      event.sender.send('broadcast-reply', JSON.stringify(rInfo))
+      socket.send(msg, 0, msg.length, rInfo.port, rInfo.address); //将接收到的消息返回给客户端
+      console.log(`udp server received data: ${msg} from ${rInfo.address}:${rInfo.port}`)
+    }
+  });
+
+  // 绑定需要监听的端口
+  socket.bind({
+    address: '0.0.0.0',
+    port: 9999
+  })
+})
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+// Scheme must be registered before the app is ready
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } }
+])
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // Use pluginOptions.nodeIntegration, leave this alone
+      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      nodeIntegration: true
+    }
+  })
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    if (!process.env.IS_TEST) win.webContents.openDevTools()
+  } else {
+    createProtocol('app')
+    // Load the index.html when not in development
+    win.loadURL('app://./index.html')
+  }
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    try {
+      await installExtension(VUEJS_DEVTOOLS)
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString())
+    }
+  }
+  createWindow()
+})
+
+// Exit cleanly on request from parent process in development mode.
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
+        app.quit()
+      }
+    })
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit()
+    })
+  }
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/Main.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    const ipcRenderer = window.electron.ipcRenderer
+    ipcRenderer.send('receiving-broadcast')
+    ipcRenderer.on('broadcast-reply', (event, arg) => {
+      console.log(arg)
+    })
+  }
+}
+</script>
